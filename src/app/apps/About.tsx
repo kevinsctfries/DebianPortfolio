b8580b89452081964bb6607ea0cefc5f274d9e5b
Kevin Fries <kevinsctfries@gmail.com>
2025-09-22 19:18:06 -0400

    update About section with lorems

--- src/app/apps/About.tsx
+++ src/app/apps/About.tsx
@@ -1,13 +1,25 @@
+import styles from "../styles/apps.module.scss";
+
 export default function About() {
   return (
-    <div>
+    <div className={styles.about}>
       <h2>About Me</h2>
       <p>
-        Hello, my name is Kevin. I am a full-stack developer who loves building
-        tools and playful interfaces.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta culpa id
+        unde vero dolores itaque quis rerum hic corporis nulla ratione nam,
+        dolorem deserunt ab! Repellat dolor quibusdam veniam nam?
       </p>
+      <br />
       <p>
-        This portfolio is styled after Debian XFCE — because Linux is home ❤️
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+        necessitatibus enim ipsam blanditiis, vel ex ut nobis molestias sequi
+        qui at illum modi in fuga natus esse hic repellat cum!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+        consectetur reiciendis modi libero provident porro ab totam sit nisi
+        quos iure esse magnam optio, soluta ut earum illum animi. Reprehenderit!
       </p>
     </div>
   );
