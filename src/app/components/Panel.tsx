9d87c6f9b83516905d3aedcdf89e434de3c86b31
Kevin Fries <kevinsctfries@gmail.com>
2025-09-23 19:02:41 -0400

    add feat for displaying opened apps in the panel

--- src/app/components/Panel.tsx
+++ src/app/components/Panel.tsx
@@ -3,9 +3,25 @@
 import { useState } from "react";
 import styles from "../styles/panel.module.scss";
 import Menu from "./Menu";
+import { useDesktop } from "./DesktopContext";
+import Image from "next/image";
 
 export default function Panel() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { openApps, openApp } = useDesktop();
+
+  const getAppIcon = (appId: string) => {
+    switch (appId) {
+      case "about":
+        return "/gnome-info.webp";
+      case "terminal":
+        return "/gnome-eterm.webp";
+      case "minesweeper":
+        return "/minesweeper.webp";
+      default:
+        return "/gnome-info.webp";
+    }
+  };
 
   return (
     <div className={styles.panel}>
@@ -26,6 +42,19 @@ export default function Panel() {
           }}
         />
       )}
+
+      <div className={styles.openedApps}>
+        {openApps.map(appId => (
+          <button
+            key={appId}
+            className={styles.appButton}
+            onClick={() => openApp(appId)}
+            title={appId}>
+            <Image src={getAppIcon(appId)} alt={appId} width={20} height={20} />
+            <span className={styles.appName}>{appId}</span>
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
